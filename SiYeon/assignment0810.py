963f958ca5fd67c8ba6ac348cb89d3d2022a6601
SYK <siyeon271@gmail.com>
2019-08-19 16:21:00 +0900

    assignment0810

---
+++ SiYeon/assignment0810.py
@@ -0,0 +1,19 @@
+
+## 0810
+#1. my_score 총합, 평균
+
+my_scores = [30, 90, 80, 40, 50]
+
+total_score = 0
+
+for i in my_scores:
+    total_score += i
+
+print(total_score)
+
+average = total_score/len(my_scores)
+
+print(average)
+
+
+
