ccafe92f8e86bcde9c067f286aa6b980d9ef38cc
leebomin <codavincee@gmail.com>
2019-08-19 15:24:22 +0900

    Refactoring with dictionary

--- bominlee/python/0818assingment/scores_grade.py
+++ bominlee/python/0818assingment/scores_grade.py
@@ -1,17 +1,14 @@
 # second process
-import string
 
 
 def get_grade(scores, subject):
-    g = ["A", "B", "C", "D", "F"]
-    score_gap = list(range(80, -1, -20))
+    grades = {"A": 80, "B": 60, "C": 40, "D": 20, "F": 0}
 
-    for i in list(range(0, 5)):
-        print(scores, i)
-        if scores[subject] > score_gap[i] :
-            ## 문제 이상해. 20 초과면 D학점이겠지???
-            print(i)
-            return g[i]
+    for key, value in grades.items():
+        print(scores, key, value)
+        if scores[subject] >= value:
+            print(key)
+            return key
 
 
 print(get_grade({'국어': 20, '영어': 50, '수학': 70}, '영어'))
